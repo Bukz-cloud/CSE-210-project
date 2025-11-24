7690e47bc7cdcefc9576f1db41d35b57afa3efd6
Bukz-cloud <bukadejohn@gmail.com>
2025-11-25 00:49:35 +0200

    I completed the homework assignment by creating Assignment, MathAssignment, and Writing Assignment cs files.

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection = "";
+    private string _problem = "";
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problem) : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problem = problem;
+    }
+    public string GetHomeWorkList()
+    {
+        return $"Booksection: {_textbookSection}, Problem: {_problem}.";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Mary Waters", "European History");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeWorkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("John Doe", "Narrative Essay", "Biafran Civil War");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
