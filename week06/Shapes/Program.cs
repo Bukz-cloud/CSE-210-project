b7a57f6a2bef97edc01149f1f30c04d075d182bb
Bukz-cloud <bukadejohn@gmail.com>
2025-12-01 22:52:31 +0200

    I completed the shape assignment in week06 folder.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapeOne = new List<Shape>();
+
+        Square squ1 = new Square("Red", 3);
+        shapeOne.Add(squ1);
+
+        Rectangle rect1 = new Rectangle("Yellow", 7, 6);
+        shapeOne.Add(rect1);
+
+        Circle cir1 = new Circle("Orange", 7);
+        shapeOne.Add(cir1);
+
+        foreach(Shape shap in shapeOne)
+        {
+            string color = shap.GetColor();
+
+            double area = shap.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area {area}");
+        }
     }
 }
